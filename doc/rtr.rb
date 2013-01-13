2d5f20b53ae83a7c2d2e1c7c1d498a4778091373
Andreas Nordahl <andnorda@gmail.com>
2013-01-13 18:28:31 +0100

    blue, nav and some styling

--- doc/rtr.rb
+++ doc/rtr.rb
@@ -1,12 +1,34 @@
-position = 0
+position = 0;
+u_position = 0;
+name = "";
+hybrid = false;
+color = ""
 File.open("rtr seed", "w") do |output|
   File.open("rtr spoiler") do |file|
     file.each do |line|
       if line.start_with? "Name:"
-        position += 1;
         name = line.split(%r{\t})[1].strip.split(%r{[ -]}).each{|word| word.capitalize!}.join(" ").delete(" ',-");
-        output << "Card.create({ :name => \""<< name <<"\", :position => \""<< position << "\", :set => 'rtr', :color => 'all'}) \n"
+      end
+      if line.start_with? "Cost:"
+        if line.include? "/"
+          hybrid = true;
+        else
+          hybrid = false;
+        end
+        color = ""
+        line.split("").each{ |char| if char[/[WUBGR]/] and !color.include? char then color << char end};
+      end
+      if line.start_with?("Set/Rarity:") and !line.strip.end_with?("Rare")
+        position += 1;
+        output << "Card.create({ :name => \""<< name <<"\", :position => \""<< position << "\", :set => 'rtr', :color => 'all'}) \n";
+        if color == "U" or (color.include? "U" and hybrid) or color == ""
+          u_position += 1;
+          output << "Card.create({ :name => \""<< name <<"\", :position => \""<< u_position << "\", :set => 'rtr', :color => 'U'}) \n"
+        end
       end
     end
   end
 end
+
+
+#Cost:	 1(B/R)(B/R)
