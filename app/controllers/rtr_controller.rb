ad90cd3cec08d9f853115ec681a7dcc9ab4a6467
Andreas Nordahl <andnorda@gmail.com>
2013-01-13 14:47:50 +0100

    make heroku work (hopefully)

--- app/controllers/rtr_controller.rb
+++ app/controllers/rtr_controller.rb
@@ -1,7 +1,7 @@
 class RtrController < ApplicationController
   def index
     if params[:color]
-      @cards = [Card.find 1] 
+      @cards = [Card.find(1)] 
     else
       @cards = Card.all
     end
