07628a8032b1cddbf389ebc0b8cd46c66200ab3b
Andreas Nordahl <andnorda@gmail.com>
2013-01-13 16:06:50 +0100

    images in view

--- doc/rtr.rb
+++ doc/rtr.rb
@@ -4,7 +4,7 @@ File.open("rtr seed", "w") do |output|
     file.each do |line|
       if line.start_with? "Name:"
         position += 1;
-        name = line.split(%r{\t})[1].strip;
+        name = line.split(%r{\t})[1].strip.split(%r{[ -]}).each{|word| word.capitalize!}.join(" ").delete(" ',-");
         output << "Card.create({ :name => \""<< name <<"\", :position => \""<< position << "\", :set => 'rtr', :color => 'all'}) \n"
       end
     end
